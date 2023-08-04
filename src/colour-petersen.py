d3cd36445f99b315d2c504b7229a621b79f7375e
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-08-04 13:27:27 +0000

    random list assignment and colours

--- src/colour-petersen.py
+++ src/colour-petersen.py
@@ -4,22 +4,20 @@ import networkx as nx
 
 G = nx.petersen_graph()
 
-print(G.edges)
+# create a global list of colours
+colours = range(0, 10)
 
-print(list(nx.edge_dfs(G, source=0)))
+# assign a random subset of colours to the list of permissible colours for every edge
+for u, v, permissible in G.edges.data("permissible"):
+    G[u][v]["permissible"] = random.sample(colours, 3)
 
+# assign a random colour to every edge
 for u, v, colour in G.edges.data("colour"):
-    G[u][v]["colour"] = random.randint(0, 3)
+    G[u][v]["colour"] = random.choice(colours)
 
-print(G.edges)
-print(G.edges.data("colour"))
-
+# iterate through nodes and neighbours printing permissible lists and colours
 for n, nbrs in G.adj.items():
    for nbr, eattr in nbrs.items():
-       wt = eattr['colour']
-       print(f"({n}, {nbr}, {wt})")
-
-def used_at(G, v):
-    return(True)
-
-print(used_at(G, 0))
+       perm = eattr['permissible']
+       col = eattr['colour']
+       print(f"({n}, {nbr}, {perm}, {col})")
