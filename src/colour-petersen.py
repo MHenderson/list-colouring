ea96d078c1bd1dc3f232c3935412c97182a01519
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-08-04 15:25:36 +0000

    this appears to be a working implementation

--- src/colour-petersen.py
+++ src/colour-petersen.py
@@ -2,18 +2,24 @@ import random
 
 import networkx as nx
 
+random.seed(0)
+
 G = nx.petersen_graph()
 
 # create a global list of colours
-colours = range(0, 10)
+colours = range(0, 3)
 
 # assign a random subset of colours to the list of permissible colours for every edge
 for u, v, permissible in G.edges.data("permissible"):
     G[u][v]["permissible"] = random.sample(colours, 3)
 
+# initialise the colour of every edge to None
+for u, v, colour in G.edges.data("colour"):
+    G[u][v]["colour"] = None
+
 # colours_incident_with
 def colours_incident_with(G, u):
-    return([1, 2, 3, 4, 5])
+    return(set([G[u][v]["colour"] for v in nx.neighbors(G, u)]))
 
 # first_permissible_or_none
 # given G, u and v this function returns the first element of A if A is not empty otherwise None
@@ -22,8 +28,8 @@ def colours_incident_with(G, u):
 #       Y is the list of colours on edges incident with v
 #       P is the list of permissible colours for edge uv
 def first_permissible_or_none(G, u, v):
-    X = set(colours_incident_with(G, u))
-    Y = set(colours_incident_with(G, v))
+    X = colours_incident_with(G, u)
+    Y = colours_incident_with(G, v)
     P = set(G[u][v]["permissible"])
     choices = P - X.union(Y)
     if(len(choices) > 0):
@@ -42,9 +48,3 @@ for n, nbrs in G.adj.items():
        perm = eattr['permissible']
        col = eattr['colour']
        print(f"({n}, {nbr}, {perm}, {col})")
-
-print(colours_incident_with(G, 4))
-
-print(nx.edges(G, 3))
-
-print([v for v in nx.neighbors(G, 3)])
