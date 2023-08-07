082d670c34bc468ccea994527ae1925b664cb668
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-08-07 14:21:35 +0000

    move test code into readme

--- tests/test_petersen.py
+++ tests/test_petersen.py
@@ -10,11 +10,4 @@ def test():
     G = greedy_list_edge_colouring(G)
 
 def test_draw():
-    G = nx.petersen_graph()
-    G = list_init(G, range(0, 10), 3, 0)
-    G = greedy_list_edge_colouring(G)
-    options = {'with_labels': True, 'node_color': "white"}
-    # this won't work if there is an uncoloured edge (i.e. None)
-    colors = nx.get_edge_attributes(G,'colour').values()
-    nx.draw_shell(G, nlist = [range(5, 10), range(5)], edge_color = colors, **options)
-    #plt.savefig("img/petersen-shell.png", format = "PNG")
+    
