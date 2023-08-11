742de371ff3d87ae58feadc713a0524497786ac0
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-08-11 14:57:35 +0100

    initialise list-assignments with list sizes other than 3 (#13)

--- tests/test_petersen.py
+++ tests/test_petersen.py
@@ -1,13 +1,11 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 
 import listcolouring
-from listcolouring import list_init, greedy_list_edge_colouring, print_list_edge_colouring
+from listcolouring import list_init, greedy_list_edge_colouring
 
 def test():
     G = nx.petersen_graph()
     G = list_init(G, range(0, 10), 3, 0)
     G = greedy_list_edge_colouring(G)
 
-def test_draw():
     
