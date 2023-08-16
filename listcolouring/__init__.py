c45a220dd846c63d93c6acddedf977d88103c469
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-08-16 10:03:17 +0000

    add docstrings

--- listcolouring/__init__.py
+++ listcolouring/__init__.py
@@ -3,28 +3,28 @@ import random
 import networkx as nx
 
 def list_init(G, colours, k, seed):
+    """Assign a random subset of k colours to the list of permissible colours for every edge of G."""
     random.seed(seed)
-    # assign a random subset of colours to the list of permissible colours for every edge
     for u, v, permissible in G.edges.data("permissible"):
         G[u][v]["permissible"] = random.sample(colours, k)
         
-    # initialise the colour of every edge to None
     for u, v, colour in G.edges.data("colour"):
         G[u][v]["colour"] = None
 
     return(G)
 
-# colours_incident_with
 def colours_incident_with(G, u):
+    """The list of colours on edges incident with vertex u in graph G."""
     return(set([G[u][v]["colour"] for v in nx.neighbors(G, u)]))
 
-# first_permissible_or_none
-# given G, u and v this function returns the first element of A if A is not empty otherwise None
-# A is P minus (X union Y)
-# where X is the list of colours on edges incident with u
-#       Y is the list of colours on edges incident with v
-#       P is the list of permissible colours for edge uv
 def first_permissible_or_none(G, u, v):
+    """
+    Returns the first element of A if A is non-empty otherwise returns None.
+    Where A is P minus (X union Y).
+    X is the list of colours on edges incident with u.
+    Y is the list of colours on edges incident with v.
+    P is the list of permissible colours for edge uv.
+    """
     X = colours_incident_with(G, u)
     Y = colours_incident_with(G, v)
     P = set(G[u][v]["permissible"])
@@ -36,13 +36,14 @@ def first_permissible_or_none(G, u, v):
     return(choice)
 
 def greedy_list_edge_colouring(G):
-    # assign the first permissible colour to every edge (or None if all permissible colours already used on incident edges)
+    """Assign the first permissible colour to every edge (or None if all permissible
+    colours already used on incident edges)."""
     for u, v, colour in G.edges.data("colour"):
         G[u][v]["colour"] = first_permissible_or_none(G, u, v) # random.choice(colours)
     return(G)
 
 def print_list_edge_colouring(G):
-    # iterate through nodes and neighbours printing permissible lists and colours
+    """Print assigned colours and lists of permissible colours for all edges in G."""
     for n, nbrs in G.adj.items():
         for nbr, eattr in nbrs.items():
             perm = eattr['permissible']
