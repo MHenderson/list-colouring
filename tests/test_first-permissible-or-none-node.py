b0ee9b7e5a1318541a23010947c521521d2dba63
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-11-14 09:52:38 +0000

    Implement first permissible or none strategy for node colouring. (#35)
    * Implement first permissible or none strategy for node colouring.
    
    * Add colour attribute to nodes in tests.

---
+++ tests/test_first-permissible-or-none-node.py
@@ -0,0 +1,13 @@
+import networkx as nx
+
+import listcolouring
+from listcolouring import first_permissible_or_none_node
+
+def test_first_permissible_or_none_node():
+    G = nx.complete_graph(3)
+    permissible_dict = {0: [0, 1], 1: [1, 2], 2: [2, 3]}
+    nx.set_node_attributes(G, permissible_dict, "permissible")
+    nx.set_node_attributes(G, None, "colour")
+    assert first_permissible_or_none_node(G, 0) == 0
+    assert first_permissible_or_none_node(G, 1) == 1
+    assert first_permissible_or_none_node(G, 2) == 2
